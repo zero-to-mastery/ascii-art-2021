8cfa45a90dd5d1b860dcb54f453e3f4172b00431
Dylan Buchi <devybuchi@gmail.com>
2021-10-02 15:06:37 -0300

    Update community-version to use image_handler module

--- community-version.py
+++ community-version.py
@@ -14,63 +14,15 @@
 # Please change the instructions according to the fix or features contributed code. 
 # comment the contribution to make others understand easy (follow the best comment practices).
 
-
-
+import sys
 import os
-from PIL import Image
-ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
 
-def scale_image(image, new_width=100):
-    """Resizes an image preserving the aspect ratio.
-    """
-    (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
-    new_height = int(aspect_ratio * new_width)
+from image_handler import ImageHandler
 
-    new_image = image.resize((new_width, new_height))
-    return new_image
-
-def convert_to_grayscale(image):
-    return image.convert('L')
-
-def map_pixels_to_ascii_chars(image, range_width=25):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
-
-    pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
-            pixels_in_image]
-
-    return "".join(pixels_to_chars)
-
-def convert_image_to_ascii(image, new_width=100):
-    image = scale_image(image)
-    image = convert_to_grayscale(image)
-
-    pixels_to_chars = map_pixels_to_ascii_chars(image)
-    len_pixels_to_chars = len(pixels_to_chars)
-
-    image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            range(0, len_pixels_to_chars, new_width)]
-
-    return "\n".join(image_ascii)
 
 def write_to_txtfile(txt):
     with open("output.txt", "w") as text_file:
         text_file.write(txt)
-
-
-def handle_image_conversion(image_filepath):
-    try:
-        image = Image.open(image_filepath)
-    except Exception as err:
-        print(f"Unable to open image file {image_filepath}.")
-        print(err)
-    else:
-        ascii_img = convert_image_to_ascii(image)
-        return ascii_img
 
 
 def validate_file_path(path):
@@ -80,6 +32,7 @@ def validate_file_path(path):
         path = input('Enter a valid file path: ')
         validate_file_path(path)
     return path
+
 
 def validate_file_extension(path):
     allowed_extensions = ["jpg", "jpeg", "png", "bmp", "jfif", "tiff", "gif"]
@@ -93,19 +46,21 @@ def validate_file_extension(path):
     return path
 
 
-
-if __name__=='__main__':
-    import sys
-
+if __name__ == '__main__':
+    
     try:
         image_file_path = sys.argv[1]
     except IndexError:
         image_file_path = input('Enter the image file path: ')
+        
     image_file_path = validate_file_extension(image_file_path)
-    
     image_file_path = validate_file_path(image_file_path)
+    
+    ztm_logo_img_handler = ImageHandler(image_file_path)
+    
     print(image_file_path)
-    ascii_img = handle_image_conversion(image_file_path)
+    ascii_img = ztm_logo_img_handler.create_ascii_image()
+    
     print(ascii_img)
     write_to_txtfile(ascii_img)
 
