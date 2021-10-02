67a48dd57b7c979cb4d92d40e1f7b6d9b9f8db08
Talha Quddoos <talhaquddoos1@gmail.com>
2021-10-02 23:44:32 +0500

    add font menu

--- text-ascii-art/text-ascii-art.py
+++ text-ascii-art/text-ascii-art.py
@@ -27,27 +27,26 @@ parser = argparse.ArgumentParser()
 
 # Add the arguments
 parser.add_argument('-text', required=False)
-parser.add_argument('-font', required=False)
 
 args = parser.parse_args()
 
-text = getattr(args, 'text') or "ZTM"
-font = getattr(args, 'font') or 0
+text = getattr(args, 'text') or input("Please enter the text you want to print: ")
 
-font = int(font)
+print("Which font do you like?")
+for i, _font in enumerate(fonts):
+	# print 4 fonts in one line
+	end = "\n" if (i + 1) % 4 == 0 else ""
 
-if font >= len(fonts):
-	print(f"Invalid font. Please choose between 1 and {len(fonts) - 1}.")
-	print("Defaulting to standard font.\n")
-	font = 0
+	# pad the columns from their right 
+	print(f"{(i+1)}.".ljust(4) +  f"{_font}".ljust(20, " "), end=end)
+
+choice = int(input(f"\nYour choice between 1 and {len(fonts)}: "))
 
 # Choose a font from the list of fonts
-style = fonts[font]
+font = fonts[choice - 1]
 
-print(f"Font: \t {font} - {style}\n")
+print(f"Font: \t {choice} - {font}\n")
 
-fig = pyfiglet.figlet_format(text, font=style)
+fig = pyfiglet.figlet_format(text, font=font)
 
 print(fig)
-
-
