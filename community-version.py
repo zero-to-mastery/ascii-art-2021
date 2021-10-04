31949014c44279bc864f1b00f119cc51adc3e147
Dylan Buchi <devybuchi@gmail.com>
2021-10-04 07:44:09 -0300

    resolve conflicts inside community-version

--- community-version.py
+++ community-version.py
@@ -14,15 +14,16 @@
 # Please change the instructions according to the fix or features contributed code. 
 # comment the contribution to make others understand easy (follow the best comment practices).
 
+import argparse
 import sys
 import os
 
 from image_handler import ImageHandler
 
 
-def write_to_txtfile(txt):
-    with open("output.txt", "w") as text_file:
-        text_file.write(txt)
+def write_to_txtfile(image_txt, out_file):
+    with open(out_file, "w") as text_file:
+        text_file.write(image_txt)
 
 
 def validate_file_path(path):
@@ -45,11 +46,16 @@ def validate_file_extension(path):
 
     return path
 
-
 if __name__ == '__main__':
-    
+
+    parser = argparse.ArgumentParser(description="Converts images into ASCII art.", add_help=True)
+    parser.add_argument("-i", "--image", help="File path to input image", nargs=1, action="store")
+    parser.add_argument("-o", "--outfile", help="File path to output text file", nargs="?", action="store")
+
+    args = parser.parse_args()
+
     try:
-        image_file_path = sys.argv[1]
+        image_file_path = args.image[0]
     except IndexError:
         image_file_path = input('Enter the image file path: ')
         
@@ -62,5 +68,7 @@ if __name__ == '__main__':
     ascii_img = ztm_logo_img_handler.create_ascii_image()
     
     print(ascii_img)
-    write_to_txtfile(ascii_img)
+
+    if args.outfile is not None:
+        write_to_txtfile(ascii_img, args.outfile)
 
