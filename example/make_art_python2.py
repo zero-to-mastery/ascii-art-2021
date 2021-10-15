2f3370390d2ae09b256d77c61938638a732b3c0f
vineet singh <tech.vineetsingh@gmail.com>
2021-10-15 23:36:54 +0530

    good cleaning in gui.py

--- example/make_art_python2.py
+++ example/make_art_python2.py
@@ -1,13 +1,12 @@
 # this project requires Pillow installation: https://pillow.readthedocs.io/en/stable/installation.html
 
 #code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
-#Extra Task: Make this code work with Python3... you will notice you will have a few errors to fix! If you get stuck, see the answer @ make_art.py
+#code modified to work with Python 3 by @aneagoie
 from PIL import Image
+
 ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
 
 def scale_image(image, new_width=100):
-    """Resizes an image preserving the aspect ratio.
-    """
     (original_width, original_height) = image.size
     aspect_ratio = original_height/float(original_width)
     new_height = int(aspect_ratio * new_width)
@@ -19,14 +18,8 @@ def convert_to_grayscale(image):
     return image.convert('L')
 
 def map_pixels_to_ascii_chars(image, range_width=25):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
-
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
-
     pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
+    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
             pixels_in_image]
 
     return "".join(pixels_to_chars)
@@ -39,7 +32,7 @@ def convert_image_to_ascii(image, new_width=100):
     len_pixels_to_chars = len(pixels_to_chars)
 
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
+            range(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
 
@@ -47,16 +40,17 @@ def handle_image_conversion(image_filepath):
     image = None
     try:
         image = Image.open(image_filepath)
-    except Exception, e:
-        print "Unable to open image file {image_filepath}.".format(image_filepath=image_filepath)
-        print e
+    except Exception as e:
+        print(f'Unable to open image file {image_filepath}.')
+        print(e)
         return
 
     image_ascii = convert_image_to_ascii(image)
-    print image_ascii
+    print(image_ascii)
 
 if __name__=='__main__':
     import sys
 
     image_file_path = sys.argv[1]
+    print(image_file_path)
     handle_image_conversion(image_file_path)
--- gui.py
+++ gui.py
@@ -29,34 +29,71 @@ if __name__ == '__main__':
     root.geometry("900x500")
     root.title("Image to ASCII converter")
     root.configure(background="black")
-    root.resizable(0, 0)
+    #root.resizable(0, 0)
 
     def browsePic():  # Allows user to browse directories for file and provides path to entry1
         global entry1
-        tempdir = filedialog.askopenfilename(parent=root, filetypes=(
-            ('png files', '*.png'), ('jpeg files', '*.jpg'), ('all files', '*.*')), title='Please select a file')
+        tempdir = filedialog.askopenfilename(
+            parent=root, 
+            filetypes=(('png files', '*.png'), ('jpeg files', '*.jpg'), ('all files', '*.*')), 
+            title='Please select a file')
         entry1.insert(tk.END, tempdir)
 
-    Label(root, text="Image to ASCII Converter", fg="black",
-          font=("Times", 25, "bold"), width=25, pady=5, padx=25).pack()
+    Label(
+        root, 
+        text="Image to ASCII Converter", 
+        fg="black",
+        font=("Times", 25, "bold"), 
+        width=25, 
+        pady=5, 
+        padx=25
+        ).pack()
 
-    label1 = Label(root, text="Please enter the file path or choose a file:",
-                   fg="white", bg='black', font=(20))
+    label1 = Label(
+        root, 
+        text="Please enter the file path or choose a file:",
+        fg="white", 
+        bg='black', 
+        font=(20)
+        )
     label1.place(x=140, y=100)
 
-    entry1 = Entry(root, bd=5, font=(15), width=60)
+    entry1 = Entry(
+        root, 
+        bd=5, 
+        font=(15), 
+        width=60
+        )
     entry1.bind(main)
     entry1.place(x=140, y=140)
 
-    browse_btn = tk.Button(root, text='Choose File', font=("Arial", 16), pady=6,
-                           width=10, bd=-2, command=browsePic)  # Button to browse through directories
+    browse_btn = tk.Button(
+        root, 
+        text='Choose File', 
+        font=("Arial", 16), 
+        pady=6,
+        width=10, 
+        bd=-2, 
+        command=browsePic
+        )  # Button to browse through directories
     browse_btn.place(x=705, y=140)
 
-    convrt_btn = Button(root, text="Convert", font=("Arial", 20),
-                        width=10, padx=5, pady=5, command=main)
+    convrt_btn = Button(
+        root, 
+        text="Convert", 
+        font=("Arial", 20),
+        width=10, 
+        padx=5, 
+        pady=5, 
+        command=main
+        )
     convrt_btn.place(x=340, y=180)
 
-    success = Label(root, text='', bg='black')
+    success = Label(
+        root, 
+        text='', 
+        bg='black'
+        )
     success.pack(pady=5)
     success.place(x=260, y=220)
 
