08b7c3c4647c3aabbdfeeabfa753062ba1fa3ae6
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-18 20:55:01 +0530

    add-try-except
    add-try-except for routes of the app

--- app.py
+++ app.py
@@ -19,43 +19,48 @@ app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # File max size set to 4MB
 
 @app.route('/')
 def get_index():
-    return render_template('index.html')
-
+    try:
+        return render_template('index.html')
+    except:
+        return "An error occured"
 
 @app.route('/generate', methods=['GET', 'POST'])
 def generate():
-    if request.method == 'POST':
-        if ('file1' not in request.files) or (request.files['file1'].filename == ''):
-            return 'No valid file found'
+    try:
+        if request.method == 'POST':
+            if ('file1' not in request.files) or (request.files['file1'].filename == ''):
+                return 'No valid file found'
 
-        file1 = request.files['file1']
+            file1 = request.files['file1']
 
-        if file1 and is_supported(file1.filename):
-            path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file1.filename))
-            file1.save(path)
+            if file1 and is_supported(file1.filename):
+                path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file1.filename))
+                file1.save(path)
 
-            ascii_image = handle_image_conversion(path, KEY_FOLDER)
-            with open('./webapp/temp.txt', 'w') as f:
-                f.write(ascii_image)
+                ascii_image = handle_image_conversion(path, KEY_FOLDER)
+                with open('./webapp/temp.txt', 'w') as f:
+                    f.write(ascii_image)
 
-            if os.path.exists(path):
-                os.remove(path)
-            return send_from_directory(app.config['TXT_FOLDER'], 'temp.txt', as_attachment=True)
+                if os.path.exists(path):
+                    os.remove(path)
+                return send_from_directory(app.config['TXT_FOLDER'], 'temp.txt', as_attachment=True)
 
-        else:
-            return f"File must be one of: {', '.join(ALLOWED_EXTENSIONS)}"
-    
+            else:
+                return f"File must be one of: {', '.join(ALLOWED_EXTENSIONS)}"
+    except:
+        return "An error occured"
 
 
 @app.route('/ztm-logo.html', methods=['GET', 'POST'])
 def show_ztm_logo_ascii_img():
-    text_image = handle_image_conversion(DEFAULT_IMAGE_PATH, KEY_FOLDER)
-    #form request
-    if request.method == 'POST':
-        image = save_as_img(text_image, 'ztm-logo-ascii.png')
-
-    return render_template('ztm-logo.html', image=text_image)
-
+    try:
+        text_image = handle_image_conversion(DEFAULT_IMAGE_PATH, KEY_FOLDER)
+        #form request
+        if request.method == 'POST':
+            image = save_as_img(text_image, 'ztm-logo-ascii.png')
+        return render_template('ztm-logo.html', image=text_image)
+    except:
+        return "An error occured"
 
 if __name__ == '__main__':
     app.run(debug=True)
